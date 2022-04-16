0bf21049e5e78315899e310ae938b5e8f151b0d2
Mirel Alex <mirel.alxx@yahoo.com>
2022-04-16 14:17:59 +0300

    heroku✨

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
 
 const contactEmail = nodemailer.createTransport({
